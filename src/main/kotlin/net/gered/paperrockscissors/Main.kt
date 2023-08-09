50edd60db6a74265d6721162dc0e32bfb05173d0
gered <gered@blarg.ca>
2023-08-09 17:18:41 -0400

    initial functionality for playing matches and tracking results

--- src/main/kotlin/net/gered/paperrockscissors/Main.kt
+++ src/main/kotlin/net/gered/paperrockscissors/Main.kt
@@ -1,5 +1,14 @@
 package net.gered.paperrockscissors
 
 fun main(args: Array<String>) {
-    println("Hello World!")
+    var matches = Matches()
+
+    matches.playMatch(Choice.ROCK);
+    matches.playMatch(Choice.PAPER);
+    matches.playMatch(Choice.SCISSORS);
+
+    println("Played ${matches.total}:")
+    println("    - You won ${matches.wins} times")
+    println("    - I won ${matches.losses} times")
+    println("    - We tied ${matches.ties} times")
 }
--- src/main/kotlin/net/gered/paperrockscissors/Match.kt
+++ src/main/kotlin/net/gered/paperrockscissors/Match.kt
@@ -1,5 +1,7 @@
 package net.gered.paperrockscissors
 
+import org.slf4j.LoggerFactory
+
 // TODO: perhaps using "yes/no/tie" values instead would be more descriptive / intuitive ... ?
 /**
  * The winning side of a played match.
@@ -53,3 +55,45 @@ enum class Choice {
     abstract fun beats(right: Choice): Result
 }
 
+class Matches {
+    companion object {
+        private val logger = LoggerFactory.getLogger(Matches::class.java)
+    }
+
+    var wins = 0
+        private set
+    var losses = 0
+        private set
+    var ties = 0
+        private set
+
+    val total get() = wins + losses + ties
+
+    fun playMatch(playerChoice: Choice): Result {
+        val computerChoice = Choice.values().random()
+        val result = playerChoice.beats(computerChoice)
+        logger.debug(
+            "Match played with player {} against computer {} with result {}",
+            playerChoice,
+            computerChoice,
+            result
+        )
+        when (result) {
+            Result.LEFT -> {
+                logger.debug("Recording player win for match result {}", result)
+                wins += 1
+            }
+
+            Result.RIGHT -> {
+                logger.debug("Recording player loss for match result {}", result)
+                losses += 1
+            }
+
+            Result.TIE -> {
+                logger.debug("Recording tie for match result {}", result)
+                ties += 1
+            }
+        }
+        return result
+    }
+}

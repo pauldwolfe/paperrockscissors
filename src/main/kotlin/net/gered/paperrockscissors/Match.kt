f18385a4d431d205ca4890618cc0fba594f47af9
gered <gered@blarg.ca>
2023-08-09 17:08:01 -0400

    add doc comments

--- src/main/kotlin/net/gered/paperrockscissors/Match.kt
+++ src/main/kotlin/net/gered/paperrockscissors/Match.kt
@@ -1,12 +1,18 @@
 package net.gered.paperrockscissors
 
 // TODO: perhaps using "yes/no/tie" values instead would be more descriptive / intuitive ... ?
+/**
+ * The winning side of a played match.
+ */
 enum class Result {
     LEFT,
     RIGHT,
     TIE,
 }
 
+/**
+ * A player's choice in a match.
+ */
 enum class Choice {
     ROCK {
         override fun beats(right: Choice): Result {
@@ -36,5 +42,14 @@ enum class Choice {
         }
     };
 
-    abstract fun beats(right: Choice) : Result
+    /**
+     * Determines which side wins against this choice (considered the "left" side) and the other choice given
+     * (considered the "right" side, as per the argument name).
+     *
+     * @param right the other choice in the match
+     *
+     * @return the winning side of the two choices
+     */
+    abstract fun beats(right: Choice): Result
 }
+

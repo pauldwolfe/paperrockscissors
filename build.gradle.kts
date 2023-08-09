adc77d8ad740977e359cdc00020a5a02b9197b46
gered <gered@blarg.ca>
2023-08-09 16:51:07 -0400

    add shadowjar plugin for uberjar building
    not that we need it necessarily for this exercise i guess, but these
    days i feel that the vast majority of JVM projects i work on tend to
    be deployed as uberjars ...

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     kotlin("jvm") version "1.9.0"
     application
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "net.gered"
@@ -26,4 +28,14 @@ tasks.withType<KotlinCompile> {
 
 application {
     mainClass.set("MainKt")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = application.mainClass.get()
+    }
+}
+
+tasks.named<ShadowJar>("shadowJar") {
+    archiveClassifier.set("uberjar")
 }
